5c96c54af7ff149b7d7d8eaf912ce54398cd4756
Your Death <62382080+zero-src@users.noreply.github.com>
2020-09-03 20:26:25 +0300

    Update e_tasks_logger.pas

--- e_tasks_logger.pas
+++ e_tasks_logger.pas
@@ -4,17 +4,17 @@
 
 -- Part #1
 Записать на языке Pascal в дробной части до 4х цифр: 
-1) 5!           ["equals to"] 1 * 2 * 3 * 4 * 5
-2) 6,38         ["equals to"] 6.38
-3) -0,7         ["equals to"] -0.7
-4) 11⁄4         ["equals to"] 11/4
-5) -(1⁄6)       ["equals to"] -(1/6)
-6) √2           ["equals to"] sqrt(2)
-7) π            ["equals to"] 3.1415926535897932385
-8) 5*10⁶        ["equals to"] 5E6
-9) -24,8 * 10⁻⁷ ["equals to"] -24.8E-07
-10) 10⁶         ["equals to"] 1E6
-11) 1⁄100000    ["equals to"] 1/100000
+1) 5!           ["equals to"]   1 * 2 * 3 * 4 * 5
+2) 6,38         ["equals to"]   6.38
+3) -0,7         ["equals to"]   -0.7
+4) 11⁄4         ["equals to"]   11/4
+5) -(1⁄6)       ["equals to"]   -(1/6)
+6) √2           ["equals to"]   sqrt(2)
+7) π            ["equals to"]   3.1415926535897932385
+8) 5*10⁶        ["equals to"]   5E6
+9) -24,8 * 10⁻⁷ ["equals to"]   -24.8E-07
+10) 10⁶         ["equals to"]   1E6
+11) 1⁄100000    ["equals to"]   1/100000
 
 -- Part #2
 1) trunc(6.9) = 6  | round(6.9) = 7
