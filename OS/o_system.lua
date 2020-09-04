db32ffbd61e4cbbc851853dc85b3ad3784d7070b
Your Death <62382080+zero-src@users.noreply.github.com>
2020-09-04 11:58:50 +0300

    Update o_system.lua

--- OS/o_system.lua
+++ OS/o_system.lua
@@ -3,7 +3,7 @@
 --------------------------------------------------------------------------------
 1) doskey /history ["command history"]
 2) ipconfig && mspaint ["multiply commands with using "&&" like in c++"]
-3) ipconfig | clip [" '| clip' allows you to save command's output to your clipboard"]
+3) ipconfig | clip [" "| clip" allows you to save command's output to your clipboard"]
 4) sfc /scannow ["scan system files for problems w/o antivirus"]
 5) ipconfig /flushdns ["fix issues with DNS resolver cache"]
 
