5d716852edd4e7e3863c185c5626219a055d089b
Your Death <62382080+zero-src@users.noreply.github.com>
2020-09-04 11:57:58 +0300

    Update o_system.lua

--- OS/o_system.lua
+++ OS/o_system.lua
@@ -1,1 +1,10 @@
+--------------------------------------------------------------------------------
+-- Lesson #001 | cmd usage | 04.09.2020
+--------------------------------------------------------------------------------
+1) doskey /history ["command history"]
+2) ipconfig && mspaint ["multiply commands with using "&&" like in c++"]
+3) ipconfig | clip [" '| clip' allows you to save command's output to your clipboard"]
+4) sfc /scannow ["scan system files for problems w/o antivirus"]
+5) ipconfig /flushdns ["fix issues with DNS resolver cache"]
+6)
 
