086d2ed07696d108bfff74836391111c073fed5d
Your Death <62382080+zero-src@users.noreply.github.com>
2020-09-07 10:52:12 +0300

    Update a_language.cpp

--- C++/a_language.cpp
+++ C++/a_language.cpp
@@ -29,3 +29,4 @@
 
 1) https://ravesli.com/uroki-cpp/                  // крутые занятия
 2) https://code-live.ru/tag/cpp-manual/            // тут они тоже на уровне
+3) https://youtu.be/HcESuwmlHEY                    // cool channel
