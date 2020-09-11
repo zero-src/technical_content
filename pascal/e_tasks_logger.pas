c324e8b737952c87aea68f74f22de0c7dc1311b8
Your Death <62382080+zero-src@users.noreply.github.com>
2020-09-11 18:46:15 +0300

    Update e_tasks_logger.pas

--- pascal/e_tasks_logger.pas
+++ pascal/e_tasks_logger.pas
@@ -1,4 +1,20 @@
 --------------------------------------------------------------------------------
+-- Lesson #003 | Home task | 09.09.2020 
+--------------------------------------------------------------------------------
+
+-- Part #1
+1. 0 < x < 1         ["equals to"]   (0 < x) and (x < 1)
+2. x = max(x, y, z)  ["equals to"]   (x > y) and (x > z)
+3. x ≠ max(x, y, z)  ["equals to"]   ( (x > y) and (x < z) ) or ( (x < y) and (x > z) )
+4. .. a или b ..     ["equals to"]   a or b
+5. .. a и b ..       ["equals to"]   a and b
+
+-- Part #2
+1. uses math; var x:real; begin readln(x); writeln( power((power(x, 8) + power(8, x)), 1/8) ) end.
+2. uses math; var x, y, z:real; begin readln(x, y, z); writeln( (x*y*z - 3.3*abs(x + power(y, 1/4))) / (power(10, 7) + power(ln(4)/ln(10), 1/2)) ) end.
+3. uses math; begin writeln( ( Beta + sqr( sin( power(π, 4))) ) / (cos(2) + abs(cos(Gamma) / sin(Gamma))) ) end.
+
+--------------------------------------------------------------------------------
 -- Lesson #002 | Home task | 03.09.2020 
 --------------------------------------------------------------------------------
 --[[https://unicode-table.com/ru/sets/superscript-and-subscript-numbers/]]--
@@ -22,18 +38,3 @@
 2) trunc(6.2) = 6  | round(6.2) = 6
 3) round(0.5) = 1
 
---------------------------------------------------------------------------------
--- Lesson #003 | Home task | 09.09.2020 
---------------------------------------------------------------------------------
-
--- Part #1
-1. 0 < x < 1         ["equals to"]   (0 < x) and (x < 1)
-2. x = max(x, y, z)  ["equals to"]   (x > y) and (x > z)
-3. x ≠ max(x, y, z)  ["equals to"]   ( (x > y) and (x < z) ) or ( (x < y) and (x > z) )
-4. .. a или b ..     ["equals to"]   a or b
-5. .. a и b ..       ["equals to"]   a and b
-
--- Part #2
-1. uses math; var x:real; begin readln(x); writeln( power((power(x, 8) + power(8, x)), 1/8) ) end.
-2. uses math; var x, y, z:real; begin readln(x, y, z); writeln( (x*y*z - 3.3*abs(x + power(y, 1/4))) / (power(10, 7) + power(ln(4)/ln(10), 1/2)) ) end.
-3. uses math; begin writeln( ( Beta + sqr( sin( power(π, 4))) ) / (cos(2) + abs(cos(Gamma) / sin(Gamma))) ) end.
