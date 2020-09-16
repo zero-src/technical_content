21cf5b7f19fce081812f6f924011e1f5ec9ec20a
Your Death <62382080+zero-src@users.noreply.github.com>
2020-09-16 14:31:55 +0300

    Create pain.pas

---
+++ pascal/pain.pas
@@ -0,0 +1,22 @@
+program g_machine;
+
+// объявляем переменные
+var a, b, c, m_num:integer;
+
+// процедура поиска максимального числа
+procedure max(var count : integer; a, b, c : integer ); begin
+    if ( a > b ) then count := a else count := b;
+    if ( c > count ) then count := c;
+end;
+
+begin
+    // объявление переменных
+    readln(a, b, c);
+    
+    // сама процедура
+    // m_num будет принимать значение максимума
+    max(m_num, a, b, c);
+    
+    // вывод максимального числа
+    writeln(m_num);
+end.
