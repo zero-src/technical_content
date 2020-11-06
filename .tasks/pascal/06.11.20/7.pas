e9154bef0b5772a8e404cdc349056bee03c93aa2
zero-src <fabianiteam@gmail.com>
2020-11-06 14:38:20 +0300

    updated

---
+++ .tasks/pascal/06.11.20/7.pas
@@ -0,0 +1,17 @@
+﻿const LAST = 3;
+var arr: array [1..LAST] of integer; 
+    i, cnt: integer;
+    res: string[31];
+   
+begin
+  write('array: ');
+  for i := 1 to LAST do
+    read(arr[i]);
+  
+  for i := 1 to LAST do
+    if arr[i] = 100 then
+    cnt += 1; 
+      
+  res := cnt = 1 ? 'Yes' : 'More/Less than one number = 100';
+  write(res);
+end.
