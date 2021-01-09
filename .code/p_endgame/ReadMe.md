1fc4a6d0a2353f26445e2d714b80e05352f61fd8
Your Death <62382080+zero-src@users.noreply.github.com>
2021-01-09 15:26:59 +0300

    Updated

--- .code/p_endgame/ReadMe.md
+++ .code/p_endgame/ReadMe.md
@@ -3,7 +3,15 @@
 
 ## Update log
 
+### 09.01.21
+- Added test animation for local player model
+- Added test local player model
+- Added basic control function
+- Added "controller" path 
+- Renamed "unit_renderer" to "object_renderer"
+- Misure code improvements
+
 ### 29.12.20
 - Updated world generation method
 
-![killme](https://sun9-54.userapi.com/impf/c-qYN7lKe0h0fzBoNgMkSnCXqi65zXlEppTPvg/jijRHoSaDBk.jpg?size=805x337&quality=96&proxy=1&sign=fd3e6ba3189385e615985643ea732d61&type=album)
+![killme](https://media.discordapp.net/attachments/546238807838687242/797232131843424276/unknown.png?width=800&height=387)
--- .code/p_endgame/endgame.pas
+++ .code/p_endgame/endgame.pas
@@ -1,11 +1,30 @@
 ﻿uses local_animations in 'E:\endgame\animlayers\local_animations.pas';
-uses unit_renderer    in 'E:\endgame\textures\unit_renderer.pas';
-uses core, o_params;
+uses unit_renderer    in 'E:\endgame\textures\object_renderer.pas';
+uses local_player     in 'E:\endgame\controller\local_player.pas';
+uses core, o_params, GraphABC, ABCSprites, ABCObjects, Events;
+
+const link: array of string = (
+        'E:\endgame\animlayers\local_anims\',
+        'E:\endgame\animlayers',
+        'E:\endgame\controller',
+        'E:\endgame\textures',
+        'E:\endgame\'
+      );
+
+var lp: SpriteABC;
 
 begin
-  var world_renderer: world_render;
+  ///World renderer class
+  var world_renderer: world_render;  
   world_renderer := world_render.create;
   
+  ///Movement class
+  var local_movement: movement;  
+  local_movement := movement.create;
+  
   window_setup;
-  world_renderer.o_flat(15); //value in blocks
+  lp := new SpriteABC(150, 199, link[0] + 'spr.spinf');
+  
+  world_renderer.o_flat(9);
+  local_movement.ground(lp, 68);
 end.
