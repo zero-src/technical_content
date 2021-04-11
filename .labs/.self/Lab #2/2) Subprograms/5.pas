227322fb4aa17902eeb9f034d62859f36226df1c
Your Death <62382080+zero-src@users.noreply.github.com>
2021-04-11 22:59:21 +0300

    Updated

---
+++ .labs/.self/Lab #2/2) Subprograms/5.pas
@@ -0,0 +1,80 @@
+﻿const ARRAYS = 3;
+      FAIL = 0;
+      
+label HERE;
+
+/// New simplified types 
+type bool     = boolean;
+     int      = integer;
+     matrix   = array [,] of int;
+     database = array [1..ARRAYS] of matrix;
+
+/// Defining global variables
+var max_odd, id, o_n, o_m, o_k: int;
+    data: database;
+
+/// Fill arrays of matrix
+procedure data_input(var data: database);
+begin
+  for var i := 1 to ARRAYS do
+  begin
+    data[i] := new int[readinteger($'Matrix{i} [n]:'), readinteger($'Matrix{i} [m]:')];
+
+    println('Your matrix:');
+    data[i].Fill((n, m) -> readinteger);
+  end;
+end;
+
+/// Outputs required matrix
+procedure matrix_output(var data: database; idx: int; text: string := '');
+begin
+  println('-'*12 + #13 + text + ':');
+  for var i := 0 to data[idx].RowCount - 1 do
+    println(data[idx][i, :]);
+end;
+
+begin  
+  /// Data input
+  data_input(data);
+  
+   /// FAIL value
+  o_k := FAIL;
+  /// Check for odd numbers
+  for var k := 1 to ARRAYS do
+    for var i := 0 to data[k].RowCount - 1 do
+      for var j := 0 to data[k].ColCount - 1 do
+        if data[k][i, j].IsOdd then
+        begin
+          (o_n, o_m, o_k) := (i, j, k);
+          max_odd := data[k][i, j];
+          goto HERE;
+        end;
+
+  /// Check for odd numbers
+  if o_k = FAIL then
+  begin 
+    print('Odd numbers not found.');
+    exit;
+  end;
+  
+  HERE:
+  id := o_k;
+  
+  /// Max value search
+  for var i := o_k to ARRAYS do
+  begin
+    foreach var x in data[i][o_n:,o_m:] do
+      if x.IsOdd then
+      begin
+         max_odd := x > max_odd ? x : max_odd;
+    
+         if max_odd = x then
+           id := i;
+      end;
+      
+    (o_n, o_m) := (0, 0);
+  end;
+  
+  /// Matrix output
+  matrix_output(data, id, 'Final matrix');
+end.
---
+++ .labs/.self/Lab #2/2) Subprograms/6.pas
@@ -0,0 +1,51 @@
+﻿var idx: integer;
+    s, c_str: string;
+
+function rm_all_except(s: string; your_set: set of char): string;
+begin
+  for var i := s.Length downto 1 do
+    if not (s[i] in your_set) then
+      s[i] := ' ';
+    
+  result := s;
+end;
+
+begin
+  /// Ввод строки и удаление первых и последних пробелов
+  s := readstring('string:');
+  s := s.trim;
+  
+  /// Удаление лишних пробелов между словами
+  while pos(' '*2, s) > 0 do
+    delete(s, pos(' '*2, s), 1);
+  
+  /// Удаление лишних символов из строки
+  s := rm_all_except(s, ['0'..'9', '+', '-', '.']);
+  s := s.trim;
+  
+  /// Начальное значение строчки
+  c_str := '';
+  
+  /// Начало цикла
+  idx := s[1] in ['+', '-'] ? 2 : 1;
+  
+  /// Начальные значения
+  if s[1] in ['+', '-'] then
+  begin
+    s := ' ' + s;
+    idx := 2;
+  end
+  else
+    idx := 1;
+  
+  /// Добавление нужных пробелов между цифрами
+  for var i := idx to s.Length do
+    if s[i] in ['+', '-'] then
+      c_str += ' ' + s[i]
+    else
+      c_str += s[i];
+  
+  c_str := c_str.trim;
+  println('Ответ:', c_str);
+end.
+/// ab+0.1937-1.1w12
