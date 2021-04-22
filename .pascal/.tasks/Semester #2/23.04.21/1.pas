785eb10f3844b61e861fb6826e0c4981dccfc5b2
Your Death <62382080+zero-src@users.noreply.github.com>
2021-04-22 23:05:37 +0300

    Updated

---
+++ .pascal/.tasks/Semester #2/23.04.21/1.pas
@@ -0,0 +1,40 @@
+﻿var f, tmp: file of integer;
+    num, count: integer;
+
+begin
+  assign(f, '1.txt');
+  assign(tmp, '1t.txt');
+  
+  count := readinteger('num cnt:');
+  
+  rewrite(f);
+  for var i := 1 to count do
+  begin
+    seek(f, FileSize(f));
+    write(f, readinteger);
+  end;
+  close(f);
+  
+  rewrite(tmp);
+  reset(f);
+  while not eof(f) do
+  begin
+    read(f, num);
+    
+    if num >= 0 then
+      write(tmp, num);
+  end;
+  close(tmp);
+  close(f);
+  
+  rewrite(f);
+  reset(tmp);
+  while not eof(tmp) do
+  begin
+    read(tmp, num);
+    
+    write(f, num);
+  end;
+  close(tmp);
+  close(f);
+end.
---
+++ .pascal/.tasks/Semester #2/23.04.21/3.pas
@@ -0,0 +1,39 @@
+﻿var f, tmp: file of integer;
+    num, count: integer;
+
+begin
+  assign(f, '3.txt');
+  assign(tmp, '3t.txt');
+  
+  count := readinteger('num cnt:');
+  
+  rewrite(f);
+  for var i := 1 to count do
+  begin
+    seek(f, FileSize(f));
+    write(f, readinteger);
+  end;
+  close(f);
+  
+  rewrite(tmp);
+  reset(f);
+  while not eof(f) do
+  begin
+    read(f, num);
+
+    write(tmp, num*2);
+  end;
+  close(tmp);
+  close(f);
+  
+  rewrite(f);
+  reset(tmp);
+  while not eof(tmp) do
+  begin
+    read(tmp, num);
+    
+    write(f, num);
+  end;
+  close(tmp);
+  close(f);
+end.
