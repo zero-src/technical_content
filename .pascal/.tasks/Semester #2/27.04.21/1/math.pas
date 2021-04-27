1be67146af6e800403e7c745c8db78f006df0c76
Your Death <62382080+zero-src@users.noreply.github.com>
2021-04-27 09:37:28 +0300

    Updated

---
+++ .pascal/.tasks/Semester #2/27.04.21/1/math.pas
@@ -0,0 +1,13 @@
+﻿unit math;
+
+interface
+const PI  = 3.14159265359;
+
+function c_length(r: real): real;
+function c_square(r: real): real;
+
+implementation
+
+function c_length(r: real): real := 2 * PI * r;
+function c_square(r: real): real := PI * r * r;
+end.
---
+++ .pascal/.tasks/Semester #2/27.04.21/2/math.pas
@@ -0,0 +1,7 @@
+unit math;
+
+interface
+
+implementation
+
+end.
