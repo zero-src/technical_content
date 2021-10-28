8e27fb03fd9999ac52a2ece454167350e78e7c3d
zero-src <62382080+zero-src@users.noreply.github.com>
2021-10-29 00:14:03 +0300

    Added main table

--- o_projects/c_simplex_method/main.cpp
+++ o_projects/c_simplex_method/main.cpp
@@ -8,11 +8,38 @@ int main()
 
     // Equation: start
     e->setupParams();
-    e->printParams();
+    e->printParamsAsTable();
 
     // Resriction: start / normalize
     e->setupRestrictions();
-    e->printRestrictions();
+    e->setupNormalizedRestrictions();
+    e->printRestrictionsAsTable();
+
+    // Main part
+    std::vector<std::string> table_sections { "Bv", "Cbv" };
+    std::vector<std::string> table_results {};
+    std::vector<std::string> table_delta {};
+
+    uint16_t param_count(e->normalized_restricts()[0].size());
+    uint16_t s_count(e->normalized_restricts().size());
+    uint16_t x_count(param_count - s_count - 1);
+
+    for (uint16_t i = 0; i < param_count; i++)
+    {
+        if (i <= x_count)
+            table_sections.emplace_back("x" + std::to_string(i+1));
+
+        if (i >= param_count - x_count - 1)
+            table_sections.emplace_back("s" + std::to_string(i - x_count));
+
+        if (i == param_count - 1)
+            table_sections.emplace_back("result");
+    }
+
+    table->new_row(table_sections);
+
+    table->update();
+    table->draw();
 
     return 0;
 }
