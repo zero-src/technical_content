eea41fd851ab5cf91350925d5bb60c1465ff4a5e
Your Death <62382080+zero-src@users.noreply.github.com>
2022-09-28 00:35:23 +0300

    Updated
    - Added multi-server support
    - Added '/log_in' command [supports https://mtkp.eljur.ru login:password check]

--- .projects/python/discord_timetable/main.py
+++ .projects/python/discord_timetable/main.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import json
 import discord
 from discord.ext.commands import has_permissions
 from datetime import datetime, timedelta
@@ -22,17 +25,22 @@ class Timetable:
             title=self.__timetable_date
         )
 
-    def channel_location(self, channel):
-        self.__timetable_cache["channel"] = channel
+    # Getter
+    def get_cache(self):
+        return self.__timetable_cache
 
-    def get_channel(self):
-        return self.__timetable_cache["channel"]
+    def get_message(self, guild_id):
+        return self.__timetable_cache[str(guild_id) + "message"]
 
-    def message_location(self, message):
-        self.__timetable_cache["message"] = message
+    def get_channel(self, guild_id):
+        return self.__timetable_cache[str(guild_id) + "channel"]
 
-    def get_message(self):
-        return self.__timetable_cache["message"]
+    # Setter
+    def set_channel_location(self, guild_id, channel):
+        self.__timetable_cache[str(guild_id) + "channel"] = channel
+
+    def set_message_location(self, guild_id, message):
+        self.__timetable_cache[str(guild_id) + "message"] = message
 
     def datetime_current(self, *, as_str=True):
         self.__timetable_date = datetime.now()
@@ -51,20 +59,41 @@ class Timetable:
             return self.__timetable_date
 
 
+class Parser:
+    def __init__(self):
+        self.__timetable_url = 'https://mtkp.eljur.ru/journal-app/week.0'
+        self.__auth_url = 'https://mtkp.eljur.ru/ajaxauthorize'
+
+    def authorization(self, username: str, password: str):
+        session = requests.Session()
+
+        payload = {
+            'username': username,
+            'password': password
+        }
+
+        res = session.post(self.__auth_url, json=payload)
+
+        try:
+            request_status = json.loads(res.content)["errors"][0]["text"]
+        except IndexError:
+            request_status = 'success'
+
+        return session, request_status
+
+
 # heap of shit-code
 intents = discord.Intents.default().all()
 client = commands.Bot(intents=intents, command_prefix="::")
 
-all_active_channels = {}
 Timetable = Timetable()
 
 
 @tasks.loop(hours=1)
 async def update():
-    # TODO: update func
-    for channel in all_active_channels.keys():
+    for channel in Timetable.get_cache().keys():
         var = client.get_channel(channel)
-        message = await var.fetch_message(all_active_channels[channel])
+        message = await var.fetch_message(Timetable.get_channel(channel))
         print(message)
 
 
@@ -83,33 +112,47 @@ async def on_ready():
     update.start()
 
 
+@client.tree.command(name="log_in", description="Receive personal data from e-journal. Login + Password required")
+async def visibility_check(interaction: discord.Interaction, login: str, password: str):
+    parser = Parser()
+    session, status = parser.authorization(login, password)
+
+    await interaction.response.send_message(
+        f'`login`**:**`password` -> `{login}`**:**`{password}`\n'
+        f'`status` {status}',
+        ephemeral=True
+    )
+
+
 @client.tree.command(name="timetable_channel", description="Setup bot in 'X' channel")
 @has_permissions(administrator=True)
 async def timetable_channel(interaction: discord.Interaction, channel: discord.TextChannel):
+    guild_id = interaction.guild_id
     Timetable.embed.title = Timetable.datetime_current()
 
     message = await channel.send(embed=Timetable.embed)
-    all_active_channels[channel.id] = message.id
 
-    Timetable.channel_location(channel)
-    Timetable.message_location(message)
+    Timetable.set_channel_location(guild_id, channel)
+    Timetable.set_message_location(guild_id, message)
 
-    await interaction.response.send_message("falco joined squad", ephemeral=True)
+    await interaction.response.send_message("falco joined server", ephemeral=True)
 
 
 @client.tree.command(name="today", description="Timetable for today")
 async def cur_day(interaction: discord.Interaction):
+    guild_id = interaction.guild_id
     Timetable.embed.title = Timetable.datetime_current()
 
-    await Timetable.get_message().edit(embed=Timetable.embed)
+    await Timetable.get_message(guild_id).edit(embed=Timetable.embed)
     await interaction.response.send_message("today", ephemeral=True)
 
 
 @client.tree.command(name="next_day", description="Timetable for next day")
 async def next_day(interaction: discord.Interaction):
+    guild_id = interaction.guild_id
     Timetable.embed.title = Timetable.datetime_manip(1)
 
-    await Timetable.get_message().edit(embed=Timetable.embed)
+    await Timetable.get_message(guild_id).edit(embed=Timetable.embed)
     await interaction.response.send_message("next_day", ephemeral=True)
 
 
